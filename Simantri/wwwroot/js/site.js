ff60ae76d03a6de781be1a37c86fd933e9934656
Fathurahman Ma'ruf <fathur.mh@gmail.com>
2019-10-21 16:05:25 +0700

    Modify the navbar.

--- Simantri/wwwroot/js/site.js
+++ Simantri/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function initClock() {
+    function updateClock() {
+        var d = new Date(),
+            second = d.getSeconds().toString().length === 1 ? '0' + d.getSeconds() : d.getSeconds(),
+            minutes = d.getMinutes().toString().length === 1 ? '0' + d.getMinutes() : d.getMinutes(),
+            hours = d.getHours().toString().length === 1 ? '0' + d.getHours() : d.getHours(),
+            //ampm = d.getHours() >= 12 ? 'pm' : 'am',
+            months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            days = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+        document.getElementById("time").innerHTML = days[d.getDay()] + ', ' + d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear() + ' ' + hours + ':' + minutes + ':' + second;
+    }
+
+    updateClock();
+    setInterval(updateClock, 1000);
+}
+
+initClock();
